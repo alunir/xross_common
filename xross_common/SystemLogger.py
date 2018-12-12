dba95c66d2b96f76e0b51f80a5a89d24f00d021d
jimako <nsplat@gmail.com>
2018-12-12 21:24:42 +0900

    modified SystemLogger to add trace and verbose logging level

--- xross_common/SystemLogger.py
+++ xross_common/SystemLogger.py
@@ -12,9 +12,12 @@ from multiprocessing_logging import MultiProcessingHandler
 
 from xross_common.SystemUtil import SystemUtil
 
+TRACE_LEVEL_NUM = 5
+VERBOSE_LEVEL_NUM = 15
+
 
 class SystemLogger(logging.getLoggerClass()):
-    def __init__(self, obj_name):
+    def __init__(self, obj_name, level="DEBUG"):
         """
         :param obj_name: str
         """
@@ -23,17 +26,68 @@ class SystemLogger(logging.getLoggerClass()):
         self.obj_name = str(obj_name)
         # MEMO: self.__class__.__name__ equals the name of the most offspring class.
         self.logger = logging.getLogger(self.obj_name)
-        # MEMO: Defining root logger level is essential.
-        self.logger.setLevel(logging.DEBUG)
-        # MEMO: asyncio logger level is always DEBUG.
-        logging.getLogger('asyncio').setLevel(logging.DEBUG)
-        self.levelno = self.logger.getEffectiveLevel()
 
         self.format_string = "%(asctime)s [" + self.obj_name + ":%(levelname)s] (%(processName)s) %(message)s"
         self.formatter = logging.Formatter(self.format_string)
-
         self.test_handler = TestHandler(Matcher())
 
+        self.levelno = None
+        self.set_loglevel(level)
+
+        self.register_handlers()
+
+    def trace(self, message, *args, **kws):
+        if self.logger.isEnabledFor(TRACE_LEVEL_NUM):
+            self.logger._log(TRACE_LEVEL_NUM, message, args, **kws)
+
+    def verbose(self, message, *args, **kws):
+        if self.logger.isEnabledFor(VERBOSE_LEVEL_NUM):
+            self.logger._log(VERBOSE_LEVEL_NUM, message, args, **kws)
+
+    def set_loglevel(self, default_level):
+        """
+        MEMO: add custom logger levels
+        ~ NOTSET, TRACE, DEBUG, VERBOSE, INFO, WARNING, ERROR, CRITICAL
+        ~      0,     5,    10,      15,   20,      30,    40,       50
+
+        LOG_LEVEL | SYS_ENV                          | Use for
+                  | UNITTEST | LOCAL | DOCKER | PROD |
+        ----------|----------|-------|--------|------|------------------------------------------------------------------
+        NOTSET    |          |       |        |      | Do not use
+        TRACE     | Y        |       |        |      | Private keys (e.g. SECRET_KEY, passwords...)
+        DEBUG     | Y        | Y     |        |      | Tracking logs for LOCAL TEST
+        VERBOSE   | Y        | Y     | Y      |      | Tracking logs for DEMO
+        INFO      | Y        | Y     | Y      | Y    | Basic logs for PROD
+        WARNING   | Y        | Y     | Y      | Y    | Application warning, still running
+        ERROR     | Y        | Y     | Y      | Y    | Application warning, stop the system
+        CRITICAL  | Y        | Y     | Y      | Y    | System disaster
+
+        :param level: str
+        :return: void
+        """
+
+        # MEMO: define custom logging levels
+        for name, level in [('TRACE', TRACE_LEVEL_NUM), ('VERBOSE', VERBOSE_LEVEL_NUM)]:
+            self.add_custom_logging_level(name, level)
+
+        # MEMO: Defining root logger level is essential.
+        logger_level = self.cfg.get_env("LOGGER_LEVEL", default=default_level)
+        self.levelno = logging.getLevelName(logger_level)
+        self.logger.setLevel(self.levelno)
+
+        # MEMO: asyncio logger level is always DEBUG.
+        logging.getLogger('asyncio').setLevel(logging.DEBUG)
+
+    def add_custom_logging_level(self, name, level):
+        """
+        :param name: str
+        :param level: str
+        :return: void
+        """
+        logging.addLevelName(level, name)
+        setattr(self.logger, name.lower(), getattr(self, name.lower()))
+
+    def register_handlers(self):
         # MEMO: In util module, SystemLogger will be failed in the day after start the process.
         handlers = list()
         handlers.append(self.setup_stream_handler())
@@ -53,8 +107,7 @@ class SystemLogger(logging.getLoggerClass()):
         else:
             for orig_handler in handlers:
                 self.logger.addHandler(orig_handler)
-        self.logger.info("Loaded SystemLogger for %s; LOGGER_LEVEL:%s"
-                         % (self.obj_name, logging.getLevelName(self.levelno)))
+        self.logger.info("Loaded SystemLogger LOGGER_LEVEL:%s" % logging.getLevelName(self.levelno))
 
     def get_logger(self):
         return self.logger, self.test_handler
@@ -67,7 +120,6 @@ class SystemLogger(logging.getLoggerClass()):
             prefix = "DEMO:"
         format_string = prefix + self.format_string
 
-        self.levelno = getattr(logging, self.cfg.get_env("LOGGER_LEVEL", "DEBUG"))
         stream_handler.setLevel(self.levelno)
         stream_handler.setFormatter(logging.Formatter(format_string))
 
@@ -83,7 +135,7 @@ class SystemLogger(logging.getLoggerClass()):
         logfile_name = 'latest' + self.cfg.get_env("LOGFILE_NAME_EXT", ".log")
         file_handler = logging.handlers.WatchedFileHandler(logfile_dir + '/' + logfile_name, mode='a')
 
-        file_handler.setLevel(getattr(logging, self.cfg.get_env("LOGFILE_LEVEL", "DEBUG")))
+        file_handler.setLevel(self.levelno)
         file_handler.setFormatter(self.formatter)
         return file_handler
 
