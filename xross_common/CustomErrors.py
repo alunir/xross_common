b3561757cc96320700dd3a05c3bc4517a9dc375a
jimako <nsplat@gmail.com>
2019-03-16 15:58:13 +0900

    modified CustomErrors to add NoMarketDataException

--- xross_common/CustomErrors.py
+++ xross_common/CustomErrors.py
@@ -14,13 +14,18 @@ class BaseError(Exception):
     pass
 
 
+class NoMarketDataException(BaseError):
+    """"Raised when the system couldn't fetch any market data"""
+    pass
+
+
 class UninitializedImageError(BaseError):
     """"Raised when the system couldn't initialize the image"""
     pass
 
 
 class UniqueOrderIdValidation(BaseError):
-    """"Raised when an order_id on active_db or execution_db is duplicated."""
+    """"Raised when an order_id on active_db or execution_db is duplicated"""
     pass
 
 
